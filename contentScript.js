e0c6ca2bc18d6ea4cf2bcf1fb8ddd555f41ca1b3
Carlos Cuevas <carloscuevas1@gmail.com>
2016-03-30 23:20:43 -0400

    updated to work only on profile pages

--- contentScript.js
+++ contentScript.js
@@ -1,58 +1,28 @@
 (function(){
 	'use strict';
 
-	//initialize url and selector
+	// initialize url
 	var currentUrl = location.href;
-	try {
-	    var selector = setSelector();
-	}
-	catch(error) {
-	    reportError(error);
-	}
 
+	// only need to click once
     var counter = 0;
+
+    // try to load selector in case we're already on a profile page
+    var selector = document.querySelector('#react-root > section > main > article > div > div._pupj3 > a');
 
 	window.onscroll = function() {
 	    if ((document.body.offsetHeight + document.body.scrollTop) === document.body.scrollHeight){
 	    	//if url has changed, reset currentUrl and selector
 	    	if (currentUrl !== location.href){
-
                 counter = 0;
 	    		currentUrl = location.href;
-	    		
-	    		try {
-				    selector = setSelector();
-				}
-				catch(error) {
-				    reportError(error);
-				}
-
+	    		selector = document.querySelector('#react-root > section > main > article > div > div._pupj3 > a');
 	    	}
 
-	        if (selector && (counter === 0)){
+	        if (selector && (counter === 0)) {
 	        	selector.click();
                 counter = 1;
 	        }
-	    	
 	    }        
 	};
-
-	function reportError(errorObj){
-		console.error('Instagram Infinite Scrolling Extension:', errorObj.message);
-	}
-
-	function setSelector(){
-		//check if we're on a profile page or the main feed. cache the appropriate selector.
-		var selectorTmp = document.querySelector('#react-root > section > main > article > div > div.-cx-PRIVATE-AutoloadingPostsGrid__moreLoadingIndicator > a');
-		if (!selectorTmp){
-		   selectorTmp = document.querySelector('#react-root > section > main > section > div > div.-cx-PRIVATE-AutoloadingPostsGrid__moreLoadingIndicator > a');
-		}
-
-		if (!selectorTmp){
-			throw new Error('Could not find expected DOM Element');
-		}
-
-		return selectorTmp;
-	}
-
 })();
